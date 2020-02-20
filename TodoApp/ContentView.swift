ad792ca937187c6f4d593086db9eb767492c50d3
Suresh Chaudhari <codebrust@gmail.com>
2020-02-20 16:15:56 +0545

    init

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Suresh Chaudhari on 2/20/20.
+//  Copyright © 2020 codebrust. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
